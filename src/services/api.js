80b2cfcdce3db130f9604ffbd286679523317935
Marlos11 <marlos-gama@hotmail.com>
2024-04-16 09:17:55 -0300

    feat: adição do token jwt as rotas de chamada

--- src/services/api.js
+++ src/services/api.js
@@ -6,4 +6,15 @@ const apiCodeBurguer = axios.create({
 
 })
 
+
+// estamos utilizando o axios para intercepitar a nossa requisição para verificar se o token é valido 
+// para o usuario ter acesso a pagina de categorias
+apiCodeBurguer.interceptors.request.use(async config => {
+    const userData = await localStorage.getItem('codeburger:userData')
+    const token = userData &&  JSON.parse(userData).token
+    config.headers.Authorization = `Bearer ${token}`
+
+    return config
+})
+
 export default apiCodeBurguer
