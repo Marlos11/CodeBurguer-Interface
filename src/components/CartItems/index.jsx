5e84331d76ec38bbbee6eb4a39db91c85e55306f
Marlos11 <marlos-gama@hotmail.com>
2024-08-15 14:31:12 -0300

    feat: alteracoes no componente de contexto de carrinho

--- src/components/CartItems/index.jsx
+++ src/components/CartItems/index.jsx
@@ -2,37 +2,43 @@
 import React from "react"
 
 import { Container, Header, Body } from "./styles"
-import { useCart } from "../../hooks/CartContext"
+import  {useCart}  from "../../hooks/CartContext"
 import formatCurrency from "../../utils/formatCurrency"
+
 
 
 
 export const CartItems = () => {
 
-    const { cartProducts } = useCart()
+    const {cartProducts  } = useCart() 
     console.log(cartProducts)
+    
+  
     return (
         <Container>
 
-            <Header>
-                <p></p>
+           <Header>
+                <p></p> 
                 <p>Itens</p>
                 <p>Preço</p>
                 <p>Quantidade </p>
                 <p>Total</p>
-            </Header>
+            </Header> 
 
-            {cartProducts && cartProducts.map(product => (
-
+           {cartProducts &&  cartProducts.map(product => (
+                
                 <Body key={product.id}>
                     <img src={product.url} alt="imagem-produtos" />
                     <p>{product.name}</p>
                     <p>{formatCurrency(product.price)}</p>
                     <p>{product.quantity}</p>
                     <p>{formatCurrency(product.quantity * product.price)}</p>
+                   
 
                 </Body>
-            ))}
+                
+            ))} 
+            
 
 
         </Container>
--- src/hooks/CartContext.jsx
+++ src/hooks/CartContext.jsx
@@ -10,37 +10,39 @@ export const CartProvider = ({ children }) => {
 
     const [cartProducts, setCartProducts] = useState([])
 
+
     const putProductsInCat = async product => {
 
         const cartIndex = cartProducts.findIndex(prd => prd.id === product.id)
+        let newCartProducts = cartProducts
 
-            let newCartProducts = []
         if (cartIndex >= 0) {
-
-         newCartProducts = cartProducts
-
-            newCartProducts[cartIndex] = newCartProducts[cartIndex].quantity + 1
-
+         
+            newCartProducts[cartIndex].quantity =
+                newCartProducts[cartIndex].quantity + 1
             setCartProducts(newCartProducts)
-
         } else {
             product.quantity = 1
-            newCartProducts = [...cartProducts,product]
+            newCartProducts = [...cartProducts, product]
             setCartProducts(newCartProducts)
+           
+
         }
 
-        await localStorage.setItem('codeburger:cartInfo', JSON.stringify(newCartProducts))
+        await localStorage.setItem('codeburguer:cartInfo', JSON.stringify(newCartProducts))
     }
+
 
     useEffect(() => {
 
         const loadUserData = async () => {
+           
             const clientCartData = await localStorage.getItem('codeburger:cartInfo')
 
             if (clientCartData) {
                 setCartProducts(JSON.parse(clientCartData))
             }
-
+      
         }
 
         loadUserData()
