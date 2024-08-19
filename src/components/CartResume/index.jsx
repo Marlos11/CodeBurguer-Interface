80ada2bbd098c76da834f985033c7ee4323fa228
Marlos11 <marlos-gama@hotmail.com>
2024-08-19 14:25:59 -0300

    feat:create component cartResume

---
+++ src/components/CartResume/index.jsx
@@ -0,0 +1,23 @@
+// eslint-disable-next-line no-unused-vars
+import React from "react"
+
+import { Container } from "./styles"
+
+ 
+
+
+
+
+export const CartResume = () => {
+
+
+    return (
+        <Container>
+
+           
+          <p> Olá </p>
+          
+        </Container>
+    )
+}
+
