01ba671f52ae0ef33581edc8c71d0dcca86481d7
Marlos11 <marlos-gama@hotmail.com>
2024-09-13 14:20:56 -0300

    feat:criacao da variavel para armazenamento dos status

---
+++ src/containers/Admin/Orders/orderStatus.js
@@ -0,0 +1,39 @@
+const status = [
+
+    {
+
+        id: 1,
+        label: 'Pedido realizado',
+        value: 'Pedido realizado '
+    },
+
+    {
+
+        id: 2,
+        label: 'Pedido Preparação',
+        value: 'Pedido Preparação '
+    },
+    {
+
+        id: 3,
+        label: 'Pedido pronto',
+        value: 'Pedido pronto '
+    },
+    {
+
+        id: 4,
+        label: 'Pedido á caminho',
+        value: 'Pedido á caminho '
+    },
+    {
+
+        id: 5,
+        label: 'Pedido entregue',
+        value: 'Pedido entregue'
+    }
+]
+
+
+
+
+export default status
